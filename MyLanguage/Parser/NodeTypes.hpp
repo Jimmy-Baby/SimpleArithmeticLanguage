d7d5ea0167f4ea63aad9db3a61c2c47ff0260df1
Jim Bab <xxtoohardxx@gmail.com>
2023-05-04 16:30:46 +1000

    Continue parser

---
+++ MyLanguage/Parser/NodeTypes.hpp
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "Lexer/Token.hpp"
+
+class CNumberNode
+{
+public:
+	CNumberNode(CToken* token)
+	: m_Token(token)
+	{
+	}
+
+	void Print() const
+	{
+		m_Token->Print();
+	}
+
+
+private:
+	CToken* m_Token;
+};
+
+class BinaryOpNode
+{
+public:	
+	BinaryOpNode(CNumberNode left, CToken* token, CNumberNode right)
+		: m_LeftNode(left),
+		  m_Token(token),
+		  m_RightNode(right)
+	{
+
+	}
+
+private:
+	CNumberNode m_LeftNode;
+	CToken* m_Token;
+	CNumberNode m_RightNode;
+};
--- MyLanguage/Parser/Parser.hpp
+++ MyLanguage/Parser/Parser.hpp
@@ -1,13 +1,26 @@
 ﻿#pragma once
 
+#include "NodeTypes.hpp"
+
 class CParser
 {
 	// Access functions
 public:
-	CParser() = default;
+	explicit CParser(const std::vector<CToken>& tokens)
+		: m_Tokens(tokens)
+	{
+		Advance();
+	}
+
+	void Advance()
+	{
+
+	}
 
 	// Protected functions
 protected:
 	// Member variables
 private:
+	const std::vector<CToken>& m_Tokens;
+	i32 m_TokenIndex;
 };
